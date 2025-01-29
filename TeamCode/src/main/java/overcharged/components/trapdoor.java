5ce48f03f951a253950470b7983c895355014ce8
sophiarwang <sophia.r.wang6@gmail.com>
2025-01-28 23:16:30 -0800

    trapdoor added

---
+++ TeamCode/src/main/java/overcharged/components/trapdoor.java
@@ -0,0 +1,21 @@
+package overcharged.components;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.VoltageSensor;
+
+public class trapdoor {
+
+    public OcServo trapdoor;
+    public static final float CLOSED = 54f;
+    public static final float OPEN = 164f;
+
+    public trapdoor(HardwareMap hardwareMap) {
+        trapdoor = new OcServo(hardwareMap, "trapdoor", CLOSED);
+    }
+
+    public void setPosition(float pos) { trapdoor.setPosition(pos);}
+
+    public void setInit() { trapdoor.setPosition(CLOSED); }
+
+    public void setOut() { trapdoor.setPosition(OPEN); }
+}
+
